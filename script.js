f12e46d6366813cf46bf87bec299f06319e5f4ea
rpralica <rpralica@gmail.com>
2023-02-07 08:53:33 +0100

    second commit

--- script.js
+++ script.js
@@ -8,36 +8,32 @@ const streetAddress = document.getElementById('inpAddress');
 const frmOsoba = document.getElementById('frmOsoba');
 const tableBody = document.querySelector('#tBody');
 const btnSubmit = document.getElementById('btnSubmit');
+const table = document.getElementById('table');
+let rowIndex = [1];
 class Osoba {
-  constructor(id, firstName, lastName, address) {
+  constructor(rowIndex, id, firstName, lastName, address) {
+    this.rowIndex = rowIndex;
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.address = address;
   }
   showData() {
-    Osoba.showHtml(this.id, this.firstName, this.lastName, this.address);
+    Osoba.showHtml(
+      this.rowIndex,
+      this.id,
+      this.firstName,
+      this.lastName,
+      this.address
+    );
     return this;
   }
 
-  static validateForm() {
-    if (fName.value == '') {
-      Swal.fire('Popunite ime');
-      return false;
-    } else if (lName.value == '') {
-      Swal.fire('Popunite prezime');
-      return false;
-    } else if (streetAddress.value == '') {
-      Swal.fire('Popunite adresu');
-      return false;
-    } else {
-      this.showData();
-    }
-  }
   //Setting Localstorage
   storeOsoba() {
     const allData = JSON.parse(localStorage.getItem('osobe')) ?? [];
     allData.push({
+      rowIndex: rowIndex++,
       id: this.id,
       firstName: this.firstName,
       lastName: this.lastName,
@@ -49,7 +45,13 @@ class Osoba {
   static showStorageOsoba() {
     if (localStorage.getItem('osobe')) {
       JSON.parse(localStorage.getItem('osobe')).forEach(os => {
-        Osoba.showHtml(os.id, os.firstName, os.lastName, os.address);
+        Osoba.showHtml(
+          os.rowIndex,
+          os.id,
+          os.firstName,
+          os.lastName,
+          os.address
+        );
       });
     }
   }
@@ -57,6 +59,7 @@ class Osoba {
   //! UPDATE ELEMENT
   updateOsoba(id) {
     const newItem = {
+      rowIndex: this.rowIndex,
       id: this.id,
       firstName: this.firstName,
       lastName: this.lastName,
@@ -71,12 +74,13 @@ class Osoba {
     localStorage.setItem('osobe', JSON.stringify(updatedData));
   }
 
-  static showHtml(id, firstName, lastName, address) {
+  static showHtml(rowIndex, id, firstName, lastName, address) {
     const trEl = document.createElement('tr');
     trEl.innerHTML = `
 <tr>
+<td align="center" valign="middle">${rowIndex}</td>
   <td align="center" valign="middle">${firstName}</td>
-  <td align="center" valign="middle">${lastName}</td>
+ <td align="center" valign="middle">${lastName}</td>
   <td align="center" valign="middle">${address}</td>
   <td align="center" valign="middle">
       <button class="btn btn-warning edit"  data-id="${id}">Edit</button>
@@ -96,31 +100,46 @@ frmOsoba.addEventListener('submit', function (e) {
 
   if (!contIdEdit.value) {
     const id = Math.floor(Math.random() * 10000000);
-    const sd = new Osoba(id, fName.value, lName.value, streetAddress.value);
+    const sd = new Osoba(
+      rowIndex,
+      id,
+      fName.value,
+      lName.value,
+      streetAddress.value
+    );
     sd.showData().storeOsoba();
     clearInputs();
   } else {
     const id = contIdEdit.value;
-    const newEmp = new Osoba(id, fName.value, lName.value, streetAddress.value);
+    const newEmp = new Osoba(
+      rowIndex,
+      id,
+      fName.value,
+      lName.value,
+      streetAddress.value
+    );
     newEmp.updateOsoba(id);
     btnSubmit.innerHTML = 'Submit entered data';
     tableBody.innerHTML = '';
     Osoba.showStorageOsoba();
     clearInputs();
+    rIndex();
   }
 });
 tableBody.addEventListener('click', function (e) {
   e.preventDefault();
   if (e.target.classList.contains('delete')) {
     //Remove from local storage
+    
     const id = +e.target.getAttribute('data-id');
-    console.log(+e.target.getAttribute('data-id'));
+
     const emps = JSON.parse(localStorage.getItem('osobe'));
     const newData = emps.filter(item => item.id !== id);
     localStorage.setItem('osobe', JSON.stringify(newData));
-
+   
     //Remove from DOM
     e.target.parentElement.parentElement.remove();
+    
   }
 });
 tableBody.addEventListener('click', function (e) {
@@ -146,4 +165,12 @@ tableBody.addEventListener('click', function (e) {
 });
 function clearInputs() {
   fName.value = lName.value = streetAddress.value = '';
+}
+
+function rIndex() {
+  for (var i = 1; i < table.rows.length; i++) {
+    table.rows[i].onclick = function () {
+      rIndex = this.rowIndex;
+    };
+  }
 }
